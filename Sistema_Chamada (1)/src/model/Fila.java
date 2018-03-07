1bd96fa8602fc39f1f7128261c0d6881609bfb9c
WalyssonFeisSantos <36905097+WalyssonFeisSantos@users.noreply.github.com>
2018-03-06 21:11:49 -0300

    Add files via upload

---
+++ Sistema_Chamada (1)/src/model/Fila.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Fila {
+	
+	private int id;
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	private int nm_fila;
+
+	public int getNm_fila() {
+		return nm_fila;
+	}
+
+	public void setNm_fila(int nm_fila) {
+		this.nm_fila = nm_fila;
+	}
+
+}
